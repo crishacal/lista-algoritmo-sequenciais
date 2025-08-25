61239f0be6dc72f749992bb67de7388a41d718f7
cris <email>
2025-08-25 15:01:02 -0300

    alteração

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+// Exercicio 8 - Converção para dolar
+/*
 let dolar = parseFloat(prompt("Valor em dolar (US$): "));
 let cotacao = parseFloat(prompt("Cotação em real (R$): "));
 
 let real = dolar * cotacao;
 
 alert(`O valor em real é R$ ${real.toFixed(2)}`);
+*/
+
+// Exercício 9 - Qudrado da soma de 3 valores
+
+let A = parseInt(prompt("Entre com valor de A: "));
+let B = parseInt(prompt("Entre com valor de B: "));
+let C = parseInt(prompt("Entre com valor de C: "));
+
+if (isNaN(A) || isNaN(B) || isNaN(C)){
+    alert("Entre com valores válidos.");
+    return;
+}
+
+let soma = A + B + C;
+let quadrado = soma * soma;
+
+alert(`O quadrado da soma de ${A} + ${B} + ${C} é ${quadrado}`);
