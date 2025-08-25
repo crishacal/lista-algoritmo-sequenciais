b108d17d419773209518632eefe635957a13a5ac
cris <email>
2025-08-25 16:34:22 -0300

    exercicio 12

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-// Exercicio 11 - Calcula o volume de uma esfera
+// Exercicio 12 - Sucessor e antecessor 
 
-let raio = parseFloat(prompt("Entre com o valor do raio: "));
+function anteSucessor(){
+    let num = parseInt(prompt("Entre com o valor numérico: "));
 
-if (isNaN(raio)) {
-    alert("Entre com valor numérico.");
-} else if (raio <= 0) {
-    alert("Entre com valor maior que zero.")
-} else {
-    let volume = (4/3) * 3.14159 * Math.pow(raio, 3);
-    
-    alert(`O valume da esfera é de ${volume.toFixed(2)} unidades cúbicas.`);
+    if (isNaN(num) || num <= 0) {
+        alert("Entre com valor numérico válido e maior que zero.");
+        return;
+    }
+
+    let ante = num - 1;
+    let suce = num + 1;
+
+    alert(`O antecessor de ${num} é ${ante}\n` +
+        `O sucessor de ${num} é ${suce}`
+    )
 }
+anteSucessor();
