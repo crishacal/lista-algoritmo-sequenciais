053c4423475056e216365c96f44920add757c98d
cris <email>
2025-08-25 17:00:00 -0300

    exercicio-13

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-// Exercicio 12 - Sucessor e antecessor 
+// Exercicio 13 - Teste de mesa com pseudocodigo 
 
-function anteSucessor(){
-    let num = parseInt(prompt("Entre com o valor numérico: "));
+/**
+Var
+a, b, c, soma, dif : real
+inicio
+a <- 2
+b <- 3
+c <- 4
+dif <- -5
+soma <- a+b+c-dif
+dif <- soma^2
+soma <- raizq (dif)
+a <- c
+b <- -3
+escreva (a, b, c, soma, dif)
+fim
+ */
 
-    if (isNaN(num) || num <= 0) {
-        alert("Entre com valor numérico válido e maior que zero.");
-        return;
-    }
+let a = 2;
+let b = 3;
+let c = 4;
+let dif = -5;
 
-    let ante = num - 1;
-    let suce = num + 1;
+// Calcula a soma: a + b + c - dif
+let soma = a + b + c - dif;
 
-    alert(`O antecessor de ${num} é ${ante}\n` +
-        `O sucessor de ${num} é ${suce}`
-    )
-}
-anteSucessor();
+// Calcula o quadrado da soma
+dif = soma * soma;
+
+// Calcula a raiz quadrada de dif
+soma = Math.sqrt(dif);
+
+// Atribui novos valores
+a = c;
+b = -3;
+
+// Exibe os valores finais
+console.log("a:", a, "b:", b, "c:", c, "soma:", soma, "dif:", dif);
